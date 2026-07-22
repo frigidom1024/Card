92fe7da3f45c6e5431c2510a47c3571f1381826e
LinRongQiang <3500954350@qq.com>
2026-07-22 19:38:56 +0800

    -feat: 完成卡牌场景

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,24 +1,84 @@
 extends Area2D
 
-var dragging:bool= false
 
-# Called when the node enters the scene tree for the first time.
+enum CardState {
+	NORMAL,     # 普通
+	HOVER,      # 悬浮
+	DRAGGING    # 拖拽
+}
+
+
+var state: CardState = CardState.NORMAL
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	input_pickable = true
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
-	if dragging:
+
+	if state == CardState.DRAGGING:
 		global_position = get_global_mouse_position()
 
 
+
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+
 	if event is InputEventMouseButton:
+
+		# 左键
 		if event.button_index == MOUSE_BUTTON_LEFT:
-			dragging = event.pressed
-		elif  event.button_index == MOUSE_BUTTON_RIGHT:
+
 			if event.pressed:
-				rotation_degrees+=90
-				if rotation_degrees>=360:
-					rotation_degrees=0
+				change_state(CardState.DRAGGING)
+			else:
+				change_state(CardState.NORMAL)
+
+
+		# 右键
+		elif event.button_index == MOUSE_BUTTON_RIGHT:
+
+			if event.pressed:
+				rotate_card()
+
+
+
+func change_state(new_state: CardState):
+
+	state = new_state
+
+	match state:
+
+		CardState.NORMAL:
+			z_index = 0
+			scale = Vector2.ONE
+
+
+		CardState.HOVER:
+			scale = Vector2(1.1, 1.1)
+
+
+		CardState.DRAGGING:
+			z_index = 10
+			scale = Vector2(1.1, 1.1)
+
+
+
+func rotate_card():
+
+	rotation_degrees += 90
+
+	if rotation_degrees >= 360:
+		rotation_degrees = 0
+
+
+func _on_mouse_entered():
+
+	if state == CardState.NORMAL:
+		change_state(CardState.HOVER)
+
+func _on_mouse_exited():
+
+	if state == CardState.HOVER:
+		change_state(CardState.NORMAL)
