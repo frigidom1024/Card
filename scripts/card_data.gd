04f1a669abacb3c39baa7bfc4b236d5d85639c1c
LinRongQiang <3500954350@qq.com>
2026-07-25 21:08:21 +0800

    feat: 重构CardData数据模型添加战斗属性标签系统和稀有度枚举
    - 新增 CardType/CardTag/Rarity 三个枚举
    - 添加 damage/defense/heal 战斗属性
    - 添加 tags 标签数组和 rarity 稀有度字段
    - 添加 description 描述字段

--- scripts/card_data.gd
+++ scripts/card_data.gd
@@ -1,8 +1,58 @@
 class_name CardData
 extends Resource
-@export var card_id: int = 0
-@export var card_name: String = ""
 
-func _init(p_card_id: int = 0, p_card_name: String = "") -> void:
-	card_id = p_card_id
-	card_name = p_card_name
+
+@export var card_id:int = 0
+@export var card_name:String = ""
+
+enum CardType {
+	ROOT,
+	NORMAL
+}
+
+@export var card_type:CardType = CardType.NORMAL
+
+
+# 战斗属性
+@export var damage:int = 0
+@export var defense:int = 0
+@export var heal:int = 0
+
+
+# 描述
+@export_multiline var description:String = ""
+
+
+# 标签
+enum CardTag {
+
+	# 功能
+	WEAPON,
+	DEFENSE,
+	HEAL,
+	RESOURCE,
+
+
+	# 类型
+	LOCATION,
+	CREATURE,
+	ITEM,
+	EVENT,
+
+
+	# 位面
+	HOLY,
+	DARK,
+	NATURE,
+}
+@export var tags:Array[CardTag] = []
+
+# 稀有度
+enum Rarity {
+	COMMON,
+	RARE,
+	EPIC,
+	LEGENDARY
+}
+
+@export var rarity:Rarity = Rarity.COMMON
