0dbed912702544410e7ce20ecada2b4bbd1dcadb
LinRongQiang <3500954350@qq.com>
2026-07-27 15:39:14 +0800

    重构卡牌属性渲染使用内联图标
    - 用内联 ⚔ 🛡 ❤ 图标替代 battle_attr_label 预制体实例化
    - 仅显示数值大于 0 的属性，移除冗余的 _get_battle_attrs 和 _add_attr 方法

--- scripts/card/card_view.gd
+++ scripts/card/card_view.gd
@@ -1,45 +1,35 @@
 extends ColorRect
-@onready  var labelcontainer = $LabelContainer
+
+@onready var labelcontainer = $LabelContainer
 
 var card_inst:CardInstance
-@export var battle_attr_label:PackedScene
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	card_inst=CardInstance.create_debug_card()
+	card_inst = CardInstance.create_debug_card()
 	refresh_display()
-	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
-# 刷新数据并渲染
 func refresh_display():
 	_update_battle_attr_label()
-	
-func set_value(value:CardInstance):
-	card_inst=value
+
+
+func set_value(value: CardInstance):
+	card_inst = value
+	refresh_display()
+
 
 func _update_battle_attr_label():
 	for child in labelcontainer.get_children():
 		child.queue_free()
 
-	for attr in _get_battle_attrs():
-		_add_attr(attr.value, attr.type)
-
-
-func _get_battle_attrs() -> Array[Dictionary]:
-	var data = card_inst.card_data
-	return [
-		{"value": data.damage, "type": "damage"},
-		{"value": data.heal, "type": "heal"},
-		{"value": data.defense, "type": "defense"},
-	]
-
-
-func _add_attr(value: int, type: String):
-	if value <= 0:
-		return
-	var label = battle_attr_label.instantiate()
-	label.set_and_refresh(value, type)
-	labelcontainer.add_child(label)
+	var data := card_inst.card_data
+	for entry in [
+		{"value": data.damage,  "icon": "⚔", "color": Color("red")},
+		{"value": data.defense, "icon": "🛡", "color": Color("yellow")},
+		{"value": data.heal,    "icon": "❤", "color": Color("green")},
+	]:
+		if entry.value>0:
+			var label := Label.new()
+			label.text = entry.icon + str(entry.value)
+			label.modulate = entry.color
+			labelcontainer.add_child(label)
